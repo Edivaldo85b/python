0e91208ff06abac491f1a41c9fadab8790018fa2
Edivaldo85b <165622075+Edivaldo85b@users.noreply.github.com>
2024-04-22 20:44:29 -0300

    Add files via upload

---
+++ exercicios youtube/ex003 - somando dois numeros.py
@@ -0,0 +1,14 @@
+# Crie um progrma que leia dois numeros e mostre a soma entre eles;
+Numero1 = int(input("Digite o primeiro numero: "))
+Numero2 = int(input("Digite o segundo numero: "))
+soma = Numero1 + Numero2
+
+print(f"A soma dos numeros é : {soma}")
+
+#--------- Fim do codigo ----------#
+
+#nota
+# foi criado duas variaveis para digitar os numeros
+# foi usado int para dizer que é um numero inteiro
+# foi criado a variavel soma para receber a soma dos numeros
+# o print foi formatado com a variavel soma
