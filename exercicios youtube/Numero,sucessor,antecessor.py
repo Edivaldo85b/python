62500238db3ea05ed7a08f64c1b01892d133bfb1
Edivaldo85b <165622075+Edivaldo85b@users.noreply.github.com>
2024-04-22 20:11:49 -0300

    Create Numero,sucessor,antecessor.py
    exercício 005

---
+++ exercicios youtube/Numero,sucessor,antecessor.py
@@ -0,0 +1,19 @@
+# Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e antecessor.
+numero = int(input("Digite um numero : "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+
+print(f'O numero digitado foi {numero}\nSeu antecessor é {antecessor}\nSeu sucessor é {sucessor}')
+
+#-------------- fim do codigo --------------#
+
+#-------------- nota --------------#
+# 1 passo foi criar a varial numero
+# 2 passo foi usar int para numero inteiro,usar parenteses para continuar o codigo
+# 3 passo foi usar imput para captura o teclado do usuario e abrir parenteses novamente
+# 4 passo foi escrito digite um numero dentro de aspas
+# 5 passo foi criado a variavel antecessor para receber o numero e diminuir um numero
+# 6 passo foi criado a variavel sucessor para receber o numero e somar um numero
+# 7 passo foi criar o print e formatar com colchetes para apresentar a mensagem na tela
