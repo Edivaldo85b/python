9f158283ac14e3130389d40d0b9e9ab33759792e
Edivaldo85b <165622075+Edivaldo85b@users.noreply.github.com>
2024-04-22 20:34:43 -0300

    Add files via upload

---
+++ exercicios youtube/ex002 - Respondendo usuario.py
@@ -0,0 +1,9 @@
+#faca um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+nome = input(str("Digite seu nome: "))
+print(f"É um prazer te conhecer {nome} seja bem vindo")
+
+#---------- Fim do codigo ----------#
+# nota
+# foi criado a variavel nome
+# foi usado imput para capturar o teclado
+# foi feito o print e formatado com a variavel dento de colchetes.
