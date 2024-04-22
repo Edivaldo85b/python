baa5b8a809971b6dfa7d74f778757d2666702b61
Edivaldo85b <165622075+Edivaldo85b@users.noreply.github.com>
2024-04-22 20:24:55 -0300

    Add files via upload

---
+++ exercicios youtube/ex001 ola mundo.py
@@ -0,0 +1,10 @@
+# Crie um programa e escreva ola mundo na tela
+msg = "Olá Mundo!"
+print(msg)
+
+#--------- Fim do codigo ---------#
+
+# nota
+# foi feito de duas formas:
+# a primeira forma foi escrever a msg dentro do parenteses no print
+# a segunda forma foi criar a variavel msg e colocar dentro do print
