1e2b9a69e4dc21e2a0b5c959d4e2ebbec0c0a1de
Edivaldo Bomfim <165622075+Edivaldo85b@users.noreply.github.com>
2024-04-28 16:10:52 -0300

    Add files via upload

---
+++ exercicios youtube/ex025 - procurar um string dentro da outra.py
@@ -0,0 +1,6 @@
+ # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+ 
+nome = str(input("Qual o seu nome? ")).strip()
+print(f"Seu nome tem Silva? {'SILVA' in nome.upper()}")
+
+#---------- Fim do codigo ----------#
