ddd828cdd15c832ef51399b57c9672cd0ab05ec9
Edivaldo Bomfim <165622075+Edivaldo85b@users.noreply.github.com>
2024-05-12 20:10:35 -0300

    Add files via upload

---
+++ exercicios youtube/ex047 - Contagem de pares.py
@@ -0,0 +1,8 @@
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+for contador in range(2, 51, 2):
+    print(contador, end=' ')
+print('Acabou !!!')
+
+# ---------------- fim do codigo ---------------- #
+
+# o ultimo 2 foi colocado para pular de 2 em 2 na contagem
