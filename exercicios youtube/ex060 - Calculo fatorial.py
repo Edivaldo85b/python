5f529f71726d3f9976fac9f63a09aa797ed61363
Edivaldo Bomfim <165622075+Edivaldo85b@users.noreply.github.com>
2024-05-16 20:40:59 -0300

    Add files via upload

---
+++ exercicios youtube/ex060 - Calculo fatorial.py
@@ -0,0 +1,14 @@
+# Faça um programa que leia um número qualquer e mostre o seu fatorial.
+
+# Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+from math import factorial
+numero= int(input('Digite um número para calcular seu fatorial: '))
+fact = factorial(n)
+print(f'O fatorial de {n} é {f}')
+
+# --------- fim do codigo --------- #
+
+# foi importado a biblioteca para facilitar o calculo
+# durante o video foi ensinado sem a biblioteca tbm de forma matematica
+
